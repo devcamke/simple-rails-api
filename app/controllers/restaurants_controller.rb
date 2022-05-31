86141bf7ec8fe4b093bbd04622c074d0fd27c5ae
devcamke <devjam.ke@gmail.com>
2022-05-31 17:38:42 +0300

    Add CRUD Actions

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,2 +1,46 @@
 class RestaurantsController < ApplicationController
+    before_action :set_restaurant, only: %i[update show destroy]
+
+    def index
+        @restaurants = Restaurant.all
+        render json: @restaurants
+    end
+
+    def show
+        render json: @restaurant
+    end
+
+    def create
+        @restaurant = Restaurant.new(restaurant_params)
+        if @restaurant.save
+            render json: @restaurant, status: :created
+        else
+            render json: @restaurant.errors, status: :unprocessable_entity
+        end
+    end
+
+    def update
+        @restaurant.update(restaurant_params)
+        head :no_content
+    end
+
+    def destroy
+        @restaurant.destroy
+        head :no_content
+    end
+
+    private
+
+    def set_restaurant
+        @restaurant = Restaurant.find(params[:id])
+    end
+
+    def restaurant_params
+        params.require(:restaurant).permit(:name, :restaurant_type, :address, :rating)
+    end
+    
+
+
+
+
 end
